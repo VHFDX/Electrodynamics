129379c1a226a615d011efd3dfd5c22f85f57846
VHFDX <87388918+VHFDX@users.noreply.github.com>
2022-12-13 18:04:18 +0300

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from contextlib import redirect_stdout
+import os
+
+if __name__ == '__main__':
+    x = np.linspace(-5,5,500)
+    y = -20*np.exp(-0.2*np.sqrt(0.5*x**2))-np.exp(0.5*(np.cos(2*np.pi*x)+1))+np.exp(1)+20
+    plt.plot(x,y)
+    plt.show()
+
+    if 'results' not in os.listdir():
+        os.makedirs(os.path.join(os.getcwd(), 'results'))
+
+    with open("results/results.csv", "w", encoding="utf-8") as file:
+        for i in range(len(x)):
+            file.write('{} строка: x= {}, f(x)= {}\n'.format(i+1,x[i], y[i]))
